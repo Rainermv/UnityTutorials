ef5a6d1f1ebf1f21a058c8cde2d3d47a632acf85
rainermv@gmail.com <rainermv@gmail.com>
2017-11-25 11:50:41 -0200

    Tutorial 6 - working on splines

---
+++ Assets/6 - Curves and Splines/Scripts/BezierSpline.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierSpline : MonoBehaviour {
+
+    public Vector3[] points;
+
+    public int CurveCount {
+        get {
+            return (points.Length - 1) / 3;
+        }
+    }
+
+
+    public void Reset() {
+        points = new Vector3[] {
+            new Vector3(1f,0f,0f),
+            new Vector3(2f,0f,0f),
+            new Vector3(3f,0f,0f),
+            new Vector3(4f,0f,0f)
+        };
+    }
+
+    public void AddCurve() {
+
+        Vector3 point = points[points.Length - 1];
+
+        Array.Resize(ref points, points.Length + 3); // add 3 points to the array
+
+        AddPoint(point, 3);
+        AddPoint(point, 2);
+        AddPoint(point, 1);
+
+    }
+
+    private void AddPoint(Vector3 point, int posDiff) {
+
+        point.x += 1f;
+        points[points.Length - posDiff] = point;
+
+    }
+
+
+    // Get a point in the line related to the given parametric position (0 to 1)
+    public Vector3 GetPoint(float t) {
+
+        // T is the fractional part (0 to 1) to get the interpolation value for the sline curve 
+        // where 0 is the beggining of the spline, 1 is the end
+        
+        // However, we first have to find witch curve we are in
+
+        int i; // the point in which will be returned
+        
+        // if t is 1 or more, set it to the last curve
+        if (t >= 1f) {
+
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else {
+
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= 1;
+            i *= 3;
+
+            // to get the ACTUAL points, the curve index must be multiplied by 3
+        }
+
+        Vector3 bezierLocalPoint = Bezier.GetPoint(points[i], points[i+1], points[i+2], points[i+3], t);
+
+        return transform.TransformPoint(bezierLocalPoint);
+
+    }
+
+    // the speed in which we move along the curve
+    public Vector3 GetVelocity(float t) {
+        int i;
+        if (t >= 1f) {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+
+        Vector3 localDerivative = Bezier.GetFirstDerivative(points[i], points[i+1], points[i + 2], points[i + 3], t);
+
+        // since the derivative produces a velocity vector (not a point), it should not be affected by
+        // the position of the curve, so we subtract that after transforming
+        return transform.TransformPoint(localDerivative) - transform.position;
+    }
+
+    public Vector3 GetDirection(float t) {
+        return GetVelocity(t).normalized;
+    }
+}
---
+++ Assets/Editor/6 - Curves and Splines/BezierSplineInspector.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof(BezierSpline))]
+public class BezierSplineInspector : Editor {
+
+    private const int lineSteps = 10;
+    private const float directionScale = 0.5f;
+
+    private BezierSpline spline;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+
+    private void OnSceneGUI() {
+
+        spline = target as BezierSpline;
+        handleTransform = spline.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        Vector3 p0 = ShowPoint(0);
+        for (int i = 1; i < spline.points.Length; i+=3) {
+
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i+1);
+            Vector3 p3 = ShowPoint(i+2);
+
+            // Draw direct lines in gray
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
+
+            // DrawLines();
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+
+            p0 = p3;
+        }
+
+        ShowDirections();
+
+    }
+
+
+    public override void OnInspectorGUI() {
+        //base.OnInspectorGUI();
+
+        DrawDefaultInspector();
+        spline = target as BezierSpline;
+
+        if (GUILayout.Button("Add Curve")) {
+
+            Undo.RecordObject(spline, "Add Curve");
+            spline.AddCurve();
+            EditorUtility.SetDirty(spline);
+
+        }
+
+    }
+
+    // Draw line velocity in green
+    private void ShowDirections() {
+
+        Handles.color = Color.green;
+        Vector3 point = spline.GetPoint(0f);
+
+        Handles.DrawLine(point, point + spline.GetDirection(0f) * directionScale);
+
+        for (int i = 0; i < lineSteps; i++) {
+
+            point = spline.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + spline.GetDirection(i / (float)lineSteps) * directionScale);
+        }
+    }
+
+
+    // Deprecated in favor of "DrawBezier"
+    private void DrawLines() {
+
+        Vector3 lineStart = spline.GetPoint(0f); // starting point
+
+        // Draw line veocity in green
+        Handles.color = Color.green;
+        Handles.DrawLine(lineStart, lineStart + spline.GetDirection(0f));
+
+        for (int i = 0; i < lineSteps; i++) {
+
+            // Bezier curves are parametric - 0.0 is the start 1.0 is the end
+            float linePosition = i / (float)lineSteps;
+            Vector3 lineEnd = spline.GetPoint(linePosition);
+
+
+            // Draw curve in white
+            Handles.color = Color.white;
+            Handles.DrawLine(lineStart, lineEnd);
+
+            // Draw line veocity in green
+            Handles.color = Color.green;
+            Handles.DrawLine(lineEnd, lineEnd + spline.GetDirection(i / (float)lineSteps));
+
+            lineStart = lineEnd;
+
+        }
+
+
+    }
+
+    private Vector3 ShowPoint(int index) {
+
+        Vector3 point = handleTransform.TransformPoint(spline.points[index]);
+
+        EditorGUI.BeginChangeCheck();
+        point = Handles.DoPositionHandle(point, handleRotation);
+        if (EditorGUI.EndChangeCheck()) {
+            // undo drag operations before changes
+            Undo.RecordObject(spline, "Move Point");
+            EditorUtility.SetDirty(spline);
+
+            // convert point from world coordinates (handle) to local coordinates (curve)
+            spline.points[index] = handleTransform.InverseTransformPoint(point);
+        }
+
+        return point;
+
+
+    }
+
+}
